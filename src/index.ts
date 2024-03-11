59b21df371a26c4dfcfcce19392d198ef1e68223
jonathan hamilton <longjonns@gmail.com>
2024-03-11 21:16:54 +0000

    added sockets to socket branch

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import { serve } from "@hono/node-server";
 import { Hono } from "hono";
 import { auth } from "./handlers/auth/index.js";
+import { Server } from "socket.io";
 
 const app = new Hono();
 
@@ -9,7 +10,17 @@ app.route("/auth", auth);
 const port = 4321;
 console.log(`Server is running on port ${port}`);
 
-serve({
+const server = serve({
   fetch: app.fetch,
   port,
+});
+
+const io = new Server(server, {
+  cors: {
+    origin: "*",
+  },
+});
+
+io.on("connection", (socket) => {
+  console.log("a user connected");
 });
